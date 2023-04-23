8444ed0e16da6a3ffd3d9fba28d9ce5505a34e84
Michael Stallings <stall84@gmail.com>
2023-04-23 12:33:56 -0400

    fighting typescript again for a few hours to get intellisense to work by importing a require.. smh

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+// const express = require("express");
+// const app = express();
+// const http = require("node:http").Server(app);
+// const io = require("socket.io")(http);
+
+import express = require("express");
+const app = express();
+import http = require("http");
+import io = require("socket.io");
+
+// app.use(express.static(__dirname + '/public'));
